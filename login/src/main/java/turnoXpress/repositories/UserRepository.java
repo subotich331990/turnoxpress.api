4c891ffa9c942fe46bc32a27a3c72eeb87ee65fe
Roque Scovenna <roque.scovenna@gmail.com>
2023-11-29 20:23:21 -0300

    asd

--- login/src/main/java/turnoXpress/repositories/UserRepository.java
+++ login/src/main/java/turnoXpress/repositories/UserRepository.java
@@ -2,17 +2,14 @@ package turnoXpress.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import turnoXpress.entities.User;
 
 import java.util.Optional;
 
-
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-
-    public Optional<User> findById(Long id);
-
-    @Query("SELECT n from User n where n.nombre = :nombre")
-
+    @Query("SELECT u FROM User u WHERE u.nombre = :nombre")
+    Optional<User> findByNombre(@Param("nombre") String nombre);
 }
